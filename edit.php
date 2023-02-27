f7dc6f49829e5a8575e2dda0f8144cf610fa4d9e
NoahDum <noahduminil6@gmail.com>
2023-02-27 14:49:55 +0100

    fonction edit finie, toutes les fonctionnalités marchent

--- edit.php
+++ edit.php
@@ -6,39 +6,30 @@ if (!empty($_POST)) {
     $MattressSize = trim(strip_tags($_POST["MattressSize"]));
     $price = trim(strip_tags($_POST["price"]));
     $promo = trim(strip_tags($_POST["promo"]));
-
     $errors = [];
 
 
     if (empty($name)) {
-        $errors["name"] = "Le nom du lit est obligatoire";
+        $errors["name"] = "Le nom du produit est obligatoire";
     }
 
-
-    var_dump($_FILES);
-    if (isset($_FILES["picture"]) && $_FILES["picture"]["error"] === UPLOAD_ERR_OK) {
-
-        $fileTmpPath = $_FILES["picture"]["tmp_name"];
-        $fileName = $_FILES["picture"]["name"];
-        $fileType = $_FILES["picture"]["type"];
-
+    if (isset($_FILES["inputPicture"]) && $_FILES["inputPicture"]["error"] === UPLOAD_ERR_OK) {
+        $fileTmpPath = $_FILES["inputPicture"]["tmp_name"];
+        $fileName = $_FILES["inputPicture"]["name"];
+        $fileType = $_FILES["inputPicture"]["type"];
         $fileNameArray = explode(".", $fileName);
-
         $fileExtension = end($fileNameArray);
-
         $newFileName = md5($fileName . time()) . "." . $fileExtension;
-
         $fileDestPath = "./img/{$newFileName}";
-
         $allowedTypes = array("image/jpeg", "image/png", "image/webp");
         if (in_array($fileType, $allowedTypes)) {
-
             move_uploaded_file($fileTmpPath, $fileDestPath);
         } else {
-            $errors["picture"] = "Le type de fichier est incorrect (.jpg, .png ou .webp requis)";
+            $errors["inputPicture"] = "Le type de fichier est incorrect (.jpg, .png ou .webp requis)";
         }
+    } else {
+        $errors["inputPicture"] = "Veuillez sélectionner une image";
     }
-
     if ($price < 0) {
         $errors["price"] = "Le prix ne peut être inférieur à 0";
     }
@@ -47,16 +38,15 @@ if (!empty($_POST)) {
         $errors["promo"] = "La promotion ne peut être inférieur à 0 ou suppérieur au prix ";
     }
 
-
-    // Requête d'insertion en BDD de la recette s'il n'y a aucune erreur
     if (empty($errors)) {
-        $id = $_GET['id'];
-        // Connexion à la base marmiton
+
         $dsn = "mysql:host=localhost;dbname=literie3000";
         $db = new PDO($dsn, "root", "");
 
+        $id = $_GET['id'];
 
-        $query = $db->prepare("UPDATE catalogue where id = :id (name, MattressName, picture, MattressSize, price, promo) VALUES (:name, :MattressName, :picture, :MattressSize, :price, :promo)");
+
+        $query = $db->prepare("UPDATE catalogue Set name =:name , MattressName=:MattressName, picture=:picture, MattressSize=:MattressSize, price=:price, promo=:promo where id = :id");
         $query->bindParam(':id', $id, PDO::PARAM_INT);
         $query->bindParam(":name", $name);
         $query->bindParam(":MattressName", $MattressName);
@@ -66,13 +56,8 @@ if (!empty($_POST)) {
         $query->bindParam(":promo", $promo, PDO::PARAM_INT);
 
         if ($query->execute()) {
-            // La requête s'est bien déroulée donc on redirige l'utilisateur vers la page d'accueil
             header("Location: index.php");
-        } else {
-            echo "pb en BDD";
         }
-    } else {
-        echo "il y a une erreur";
     }
 }
 $db = new PDO('mysql:host=localhost;dbname=literie3000;charset=UTF8', 'root', '');
@@ -101,57 +86,71 @@ include("templates/header.php");
             <h1>Modification du lit <?= $data["name"] ?> </h1>
 
             <form action="" method="post" enctype="multipart/form-data">
-                <div>
-                    <label for="inputPicture"> Image</label>
-                    <input type="file" name="picture" id="inputPicture" value="<?= $data["picture"] ?>">
-                    <?php
-                    if (isset($errors["picture"])) {
-                    ?>
-                        <span class="info-error"><?= $errors["picture"] ?></span>
-                    <?php
-                    }
-                    ?>
-                </div>
+    <div class="form-group">
+        <label for="inputName">Nom du produit:</label>
+        <input type="text" id="inputName" name="name" value="<?= $data["name"]?>">
+        <?php
+        if (isset($errors["name"])) {
+        ?>
+            <span class="info-error"><?= $errors["name"] ?></span>
+        <?php
+        }
+        ?>
+    </div>
 
-                <label for="inputName">Nom du lit</label>
-                <input type="text" name="name" id="inputName" value="<?= $data["name"] ?>">
+    <div class="form-group">
+        <label for="inputPicture">Photo du produit :</label>
+        <input type="file" id="inputPicture" name="inputPicture">
+        <?php
+        if (isset($errors["inputPicture"])) {
+        ?>
+            <span class="info-error"><?= $errors["inputPicture"] ?></span>
+        <?php
+        }
+        ?>
+    </div>
 
-                <label for="inputMatName">Nom du Matelas</label>
-                <input type="text" name="MattressName" id="inputMatName" value="<?= $data["MattressName"] ?>">
+    <div class="form-group">
+        <label for="inputMatName">Nom du matelas :</label>
+        <input type="text" name="MattressName" id="inputMatName" value="<?= $data["MattressName"]?>">
+    </div>
 
-                <label for="inputMatSize">Taille du Matelas</label>
-                <select name="MattressSize" id="inputMatSize">
-                    <option <?= isset($MattressSize) && $MattressSize === "90x190" ? "selected" : "" ?> value="taille1">90x190</option>
-                    <option <?= isset($MattressSize) && $MattressSize === "140x190" ? "selected" : "" ?> value="taille2">140x190</option>
-                    <option <?= isset($MattressSize) && $MattressSize === "160x200" ? "selected" : "" ?> value="taille3">160x200</option>
-                </select>
+    <div class="form-group">
+        <label for="inputMatSize">Taille du Matelas</label>
+        <select name="MattressSize" id="inputMatSize">
+            <option <?= isset($MattressSize) && $MattressSize === "90x190" ? "selected" : "" ?> value="90x190">90x190</option>
+            <option <?= isset($MattressSize) && $MattressSize === "140x190" ? "selected" : "" ?> value="140x190">140x190</option>
+            <option <?= isset($MattressSize) && $MattressSize === "160x200" ? "selected" : "" ?> value="160x200">160x200</option>
+        </select>
 
-                <div>
-                    <label for="inputPrice">Prix (en euro)</label>
-                    <input type="number" name="price" id="inputPrice" value="<?= $data["price"] ?>">
-                    <?php
-                    if (isset($errors["price"])) {
-                    ?>
-                        <span class="info-error"><?= $errors["price"] ?></span>
-                    <?php
-                    }
-                    ?>
-                </div>
+    </div>
 
-                <div>
-                    <label for="inputPromo">Prix apres promotion(en euro)</label>
-                    <input type="number" name="promo" id="inputPromo" value="<?= $data["promo"] ?>">
-                    <?php
-                    if (isset($errors["promo"])) {
-                    ?>
-                        <span class="info-error"><?= $errors["promo"] ?></span>
-                    <?php
-                    }
-                    ?>
-                </div>
+    <div>
+        <label for="inputPrice">Prix (en euro)</label>
+        <input type="number" name="price" id="inputPrice" value="<?= $data["price"] ?>">
+        <?php
+        if (isset($errors["price"])) {
+        ?>
+            <span class="info-error"><?= $errors["price"] ?></span>
+        <?php
+        }
+        ?>
+    </div>
 
-                <input type="submit" value="Valider la modification">
-            </form>
+    <div>
+        <label for="inputPromo">Prix apres promotion(en euro)</label>
+        <input type="number" name="promo" id="inputPromo" value="<?= $data["promo"] ?>">
+        <?php
+        if (isset($errors["promo"])) {
+        ?>
+            <span class="info-error"><?= $errors["promo"] ?></span>
+        <?php
+        }
+        ?>
+    </div>
+
+    <input type="submit" value="Ajouter la recette" class="btn-marmiton">
+</form>
     </div>
 <?php
         } else {
