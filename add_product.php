d561c991166ff634dc7c53d319a705d5308cbb5a
NoahDum <noahduminil6@gmail.com>
2023-02-27 16:22:18 +0100

    application finie

--- add_product.php
+++ add_product.php
@@ -7,35 +7,34 @@ if (!empty($_POST)) {
     $promo = trim(strip_tags($_POST["promo"]));
     $errors = [];
 
-    // Valider que le champ name est bien renseigné
+
     if (empty($name)) {
         $errors["name"] = "Le nom du produit est obligatoire";
     }
 
-    // Gestion de l'upload de la photo de notre recette
+
     var_dump($_FILES);
     if (isset($_FILES["inputPicture"]) && $_FILES["inputPicture"]["error"] === UPLOAD_ERR_OK) {
-        // Le fichier avec l'attribut name qui vaut picture existe et qu'il n'y a pas eu d'erreur d'upload
+
         $fileTmpPath = $_FILES["inputPicture"]["tmp_name"];
         $fileName = $_FILES["inputPicture"]["name"];
         $fileType = $_FILES["inputPicture"]["type"];
 
-        // Récupération de l'extension du fichier uploadé
+
         $fileNameArray = explode(".", $fileName);
-        // La fonction end() est très pratique pour récupérer le dernier élément d'un tableau
+
         $fileExtension = end($fileNameArray);
-        // On génère un nouveau nom de fichier pour ne pas se préoccuper des espaces, caractères accentués mais aussi si des personnes upload plusieurs images ayant le même nom
-        // L'ajout de time() permet d'être sur d'avoir un hash unique
+
         $newFileName = md5($fileName . time()) . "." . $fileExtension;
-        // Attention à vérifier que le dossier de destination est bien créé au préalable
+
         $fileDestPath = "./img/{$newFileName}";
 
         $allowedTypes = array("image/jpeg", "image/png", "image/webp");
         if (in_array($fileType, $allowedTypes)) {
-            // Le type de fichier est bien valide on peut donc ajouter le fichier à notre serveur
+
             move_uploaded_file($fileTmpPath, $fileDestPath);
         } else {
-            // Le type de fichier est incorrect
+
             $errors["inputPicture"] = "Le type de fichier est incorrect (.jpg, .png ou .webp requis)";
         }
     }
@@ -45,17 +44,13 @@ if (!empty($_POST)) {
 
     if ($promo < 0 ) {
         $errors["promo"] = "La promotion ne peut être inférieur à 0 ";
-    }else if($promo > 760){
-        $errors["promo"] = "La promotion ne peut être suppérieur au prix ";
+    }else if($promo > $price){
+        $errors["promo"] = "La promotion ne peut être supérieur au prix ";
     }
-
-    // Requête d'insertion en BDD de la recette s'il n'y a aucune erreur
     if (empty($errors)) {
-        // Connexion à la base marmiton
+
         $dsn = "mysql:host=localhost;dbname=literie3000";
         $db = new PDO($dsn, "root", "");
-
-        // La valeur attendue, pour les durées, est en seconde et non en minute
 
 
         $query = $db->prepare("INSERT INTO catalogue (name, MattressName, picture, MattressSize, price, promo) VALUES (:name, :MattressName, :picture, :MattressSize, :price, :promo)");
@@ -67,7 +62,7 @@ if (!empty($_POST)) {
         $query->bindParam(":promo", $promo, PDO::PARAM_INT);
 
         if ($query->execute()) {
-            // La requête s'est bien déroulée donc on redirige l'utilisateur vers la page d'accueil
+
             header("Location: index.php");
         }
     }
